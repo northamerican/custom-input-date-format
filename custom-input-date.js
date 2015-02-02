39b9e230c87757c2d0dd3be3b4a0ddaf9ce1cb62
Chris Bitsakis <c.bitsakis@gmail.com>
2015-02-02 00:14:53 -0500

    custom input date format

---
+++ custom-input-date.js
@@ -0,0 +1,19 @@
+;$(function($){
+    // Apply year, month and day to 
+    // css-accessible data attributes on the inputs
+    function dateInputFormats() { 
+        $('input[type=date]').each(function() {
+            var $date = $(this);
+            var date = $date.val().split('-');
+
+            $date.attr({
+                'data-year': +date[0],
+                'data-month': +date[1],
+                'data-day': +date[2]
+            });
+        });
+    }
+
+    $(dateInputFormats);
+    $('input[type=date]').change(dateInputFormats);
+})(jQuery);
